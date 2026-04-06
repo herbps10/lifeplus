3a582ee686fe30ab22e81277d809d34ee99a2a13
Herb Susmann <herbps10@gmail.com>
2026-04-06 13:24:07 -0400

    Initial commit

---
+++ R/lifeplus-package.R
@@ -0,0 +1,18 @@
+#' The 'lifeplus' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name lifeplus-package
+#' @aliases lifeplus
+#' @useDynLib lifeplus, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.7. https://mc-stan.org
+#'
+NULL
